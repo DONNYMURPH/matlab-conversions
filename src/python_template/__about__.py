c2c98f7e4cccda900745bb49e7978872ce49f7ba
DONNYMURPH <135269679+DONNYMURPH@users.noreply.github.com>
2026-08-17 11:37:35 -0500

    Initial commit

---
+++ src/python_template/__about__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present barrettMCW <mjbarrett@mcw.edu>
+#
+# SPDX-License-Identifier: MIT
+"""Version information for python_template."""
+
+__version__ = "0.0.0"
---
+++ src/python_template/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024-present barrettMCW <mjbarrett@mcw.edu>
+#
+# SPDX-License-Identifier: MIT
+"""
+This is a docstring. This is used when generating the docs.
+You should describe your module in this text.
+"""
+
+from python_template.__about__ import __version__
+from python_template.example import example
+
+__all__ = ["__version__", "example"]
